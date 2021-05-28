a711dbab2417864dacbe5e23d90dee5028319de2
juacarchi <cardenasfanoce@gmail.com>
2021-05-28 19:52:04 +0200

    NubesIncluidas

---
+++ CrimsonSouls_Project/Assets/Scripts/Clouds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public SpriteRenderer spriteInfo;
+    public float changePosition = -30;
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if (transform.position.x <= changePosition)
+        {
+            transform.position = new Vector2(70, 0);
+        }
+    }
+}
